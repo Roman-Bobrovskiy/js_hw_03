fc892a4e041d2ff096b6d961ff0597afc56a934b
RB <bobrovskiyroman@gmail.com>
2021-05-30 13:12:13 +0300

    f**k task3

---
+++ js/task-03.js
@@ -0,0 +1,34 @@
+const findBestEmployee = function (employees) {
+  let getValue = Object.values(employees);
+  let maxValue = Math.max(...getValue);
+  let bestKey =
+    Object.keys(employees)[Object.values(employees).indexOf(maxValue)];
+  return bestKey;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  })
+); // lorence
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  })
+); // mango
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  })
+); // lux
